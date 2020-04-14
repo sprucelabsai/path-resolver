138d7f67d26cbe1265e830a23c9f3ea7a2372601
liquidg3 <me@taylorrome.ro>
2020-04-13 18:59:49 -0600

    removed extra log

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 import PathResolver, { IPathResolverOptions } from './PathResolver'
-import log from './lib/log'
 export default PathResolver
 export function register(options: IPathResolverOptions) {
-	log.info(
-		`Registered PathResolver with cwd ${options.tsConfigDirs.join('\n')}`
-	)
 	new PathResolver(options)
 }
